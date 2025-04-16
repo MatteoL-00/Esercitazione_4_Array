13879f195e6e990eae0ec1b3b7b9383daf570926
MatteoL-00 <126798548+MatteoL-00@users.noreply.github.com>
2025-04-16 19:53:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
-int main()
-{
+#pragma once
+
+#include <stdio.h>
+#include "Utils.hpp"
+
+int main() {
+    data_t data = read_data("data.txt");
+    double rate_of_return = calculate_rate_of_return(data);
+    double final_value = calculate_final_value(data, rate_of_return);
+
+    print_results(data, rate_of_return, final_value);
+    write_results("result.txt", data, rate_of_return, final_value);
+
     return 0;
 }
-
